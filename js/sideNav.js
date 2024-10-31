f4f19017b70976bd65574e2857add2a9532b200c
Stein Arild <103763064+Arkuradev@users.noreply.github.com>
2024-10-31 11:53:38 +0100

    Importing code from old repo
    Importing all code to old repo as the old repo was not able to make the page live.

---
+++ js/sideNav.js
@@ -0,0 +1,10 @@
+// function to display the side menu when clicked.
+function showSideNav() {
+  const sideNav = document.querySelector(".sideNav");
+  sideNav.style.display = "flex";
+}
+// function to hide the side menu when clicked.
+function hideSideNav() {
+  const sideNav = document.querySelector(".sideNav");
+  sideNav.style.display = "none";
+}
