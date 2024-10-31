1c11dc0b43b118e2bcd43a00cb21945ba5659cde
Stein Arild <103763064+Arkuradev@users.noreply.github.com>
2024-10-31 20:19:12 +0100

    Update constants.mjs

--- js/constants.mjs
+++ js/constants.mjs
@@ -1,5 +1,7 @@
 const API_BLOG_URL = "https://v2.api.noroff.dev/blog/posts/gamerblog";
 const API_REGISTER_URL = "https://v2.api.noroff.dev/auth/register";
+const API_LOGIN_URL = "https://v2.api.noroff.dev/auth/login";
 
 export { API_BLOG_URL };
 export { API_REGISTER_URL };
+export { API_LOGIN_URL };
