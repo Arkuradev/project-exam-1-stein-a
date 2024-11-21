4b0a1d244b43ceefd32a9bc748de66dfae160972
Stein Arild <103763064+Arkuradev@users.noreply.github.com>
2024-11-21 13:47:16 +0100

    Update renderPosts.mjs
    Shows blog post title in read more link to be more descriptive

--- js/renderPosts.mjs
+++ js/renderPosts.mjs
@@ -21,7 +21,7 @@ export function renderBlogPost(posts) {
     <p>${post.body.slice(0, 100)}...</p>
     <a href="/project-exam-1-stein-a/post/blog.html?name=${authorName}&postId=${
         post.id
-      }" aria-label="Read more about ${post.title}">Read more</a>
+      }">Read more about ${post.title}</a>
     
     <hr>
     `;
